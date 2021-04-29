1ac7e9d876efa34a32e18d3e4e348bc4ace1c0b8
ErjanGeekTech <erjandyushebaev875@gmail.com>
2021-04-29 17:13:07 +0300

    Initial commit

---
+++ app/src/main/java/com/example/homeworklesson2/TwoActivity.java
@@ -0,0 +1,88 @@
+package com.example.homeworklesson2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+public class TwoActivity extends AppCompatActivity {
+
+    private static final int PERMISSION_CODE = 1000;//9:36
+
+
+    Button browser, camera;
+    ImageView imageView;
+    private final int Pick_image = 1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_two);
+        browser = findViewById(R.id.browser_btn);
+        camera = findViewById(R.id.camera_btn);
+        browser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
+                startActivity(intent);
+            }
+        });
+
+        camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+                startActivity(intent);
+            }
+        });
+
+        imageView = (ImageView)findViewById(R.id.picture_jpg);
+
+        //Связываемся с нашей кнопкой Button:
+        Button PickImage = (Button) findViewById(R.id.gallery_btn);
+        //Настраиваем для нее обработчик нажатий OnClickListener:
+        PickImage.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                //Вызываем стандартную галерею для выбора изображения с помощью Intent.ACTION_PICK:
+                Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+                //Тип получаемых объектов - image:
+                photoPickerIntent.setType("image/*");
+                //Запускаем переход с ожиданием обратного результата в виде информации об изображении:
+                startActivityForResult(photoPickerIntent, Pick_image);
+            }
+        });
+    }
+
+    //Обрабатываем результат выбора в галерее:
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+
+        switch(requestCode) {
+            case Pick_image:
+                if(resultCode == RESULT_OK){
+                    try {
+
+                        //Получаем URI изображения, преобразуем его в Bitmap
+                        //объект и отображаем в элементе ImageView нашего интерфейса:
+                        final Uri imageUri = imageReturnedIntent.getData();
+                        final InputStream imageStream = getContentResolver().openInputStream(imageUri);
+                        final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+                        imageView.setImageBitmap(selectedImage);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                }
+        }}}
